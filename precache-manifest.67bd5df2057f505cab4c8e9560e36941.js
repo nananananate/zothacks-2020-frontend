eddcbe34c10f1c7f1bd36d2ac646a844a720d3f9
nananananate <piewalker442@gmail.com>
2020-11-14 23:46:08 -0800

    Updates

---
+++ precache-manifest.67bd5df2057f505cab4c8e9560e36941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d0df62891ab47a14396210739e4075",
+    "url": "/zothacks-2020-frontend/index.html"
+  },
+  {
+    "revision": "b81e3d4c047b13e7df64",
+    "url": "/zothacks-2020-frontend/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "c7935f16d0673a87425c",
+    "url": "/zothacks-2020-frontend/static/css/main.0025e172.chunk.css"
+  },
+  {
+    "revision": "b81e3d4c047b13e7df64",
+    "url": "/zothacks-2020-frontend/static/js/2.2b4ec02f.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/zothacks-2020-frontend/static/js/2.2b4ec02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7935f16d0673a87425c",
+    "url": "/zothacks-2020-frontend/static/js/main.84704e82.chunk.js"
+  },
+  {
+    "revision": "f4379ee94c44c9bf53f6",
+    "url": "/zothacks-2020-frontend/static/js/runtime-main.0b10ceb0.js"
+  }
+]);
